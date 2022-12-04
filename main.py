0918fd24a2ef393526b991087ffd1171c1a57d7b
Anthony-Andrews <anthony.r.andrews@gmail.com>
2022-12-04 11:09:18 -0800

    fixed iteration, adding commands

--- main.py
+++ main.py
@@ -2,31 +2,50 @@ import discord # import discord.py bot api
 import os # import os dependencies
 from dotenv import load_dotenv #import dotenv
 from discord import app_commands # for use with application commands
+import wget
 
 load_dotenv() # load token from dotenv
 
-client = discord.Client(intents=discord.Intents.all()) # need to update
+class MyClient(discord.Client):
+    def __init__(self):
+        super().__init__(intents=discord.Intents().all())
+        self.synced = False
 
-@client.event
-async def on_ready():
-    print('Logged in as {0.user}'.format(client)) # bot initilization gargain
+    async def on_ready(self):
+        await self.wait_until_ready()
+        if not self.synced:
+            await tree.sync()
+            self.synced = True
+        print(f"We have logged in as {self.user}.")
+
+client = MyClient()
+tree = app_commands.CommandTree(client)
+
+@tree.command(name = "size", description = "change the size of the preview image in pixels²")
+async def self(interaction: discord.Interaction, px: str):
+    await interaction.response.send_message("ok its now"+px)
 
 @client.event
 async def on_message(message): # when a message is sent:
     if message.author == client.user: # if message is sent by a hooman continue:
         return
-    i = 0
-    for attachment in message.attachments:          # for each attachment sent by user:
-        attachment = attachment.url     # parse attachment sent by user to get url, filetype, and filename.
-        filetype = attachment.split('.')[-1]        # ^
-        file = attachment.split('/')[-1]            # ^
-        filename = file.split('.')[0]               # ^
         
-        if filetype.lower() == 'stl': # if the attachement is an stl:
+    for attachment in message.attachments:          # for each attachment sent by user:
 
-            await message.attachments[i].save(file) # save the file
+        attachStr = repr(attachment).split(r"'>")[0] # parse the object to get a string
+        filetype = attachStr.split('.')[-1].lower()  # parse the string to get the file type
 
-            print(attachment) # log url of file
+        if filetype == 'stl':                        # if the attachement is an stl:
+
+            fileurl = attachStr.split(r"url='")[-1]  # parse the string to get the url
+            file = attachStr.split('/')[-1]          # parse the string to get the file name and extension
+            filename = file.split('.')[0]            # parse the file to get just the file name
+            print("==========================================================================================================================")
+            print(fileurl)
+            print(filetype)
+            print("==========================================================================================================================") # logging for debugging
+
+            await attachment.save(file) # the bane of my exsistance
 
             render = 'stltopng /res 150 /png "C:\\Users\\antho\\GitHub VSCode Remote Repos\\STL-Viewer\\'+filename+'.png" "C:\\Users\\antho\\GitHub VSCode Remote Repos\\STL-Viewer\\'+file+'"' # parse command to be sent to renderer
 
@@ -35,6 +54,8 @@ async def on_message(message): # when a message is sent:
             await message.reply(file=discord.File(filename+'.png')) # reply to user with preview image
 
             os.remove(file)                # cleanup of temporary files
-            os.remove(filename+'.png') #Easter Egg
-    i+=1
+            os.remove(filename+'.png')     # Easter Egg
+        else:
+            pass
+
 client.run(os.getenv('BOT_TOKEN')) # start bot with token
